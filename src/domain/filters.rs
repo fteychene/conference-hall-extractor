f03b1122c44a8ed073435190999deb28ead1b156
fteychene <francois.teychene@gmail.com>
2019-04-11 22:32:27 +0200

    feat: Add filter status

--- src/domain/filters.rs
+++ src/domain/filters.rs
@@ -2,7 +2,7 @@ use crate::domain::model::{Talk, Speaker};
 use im::Vector;
 
 pub fn confirmed_talks(talk: &Talk) -> bool {
-    talk.state == "confirmed"
+    status(vector!["confirmed".to_string()])(talk)
 }
 
 pub fn not<T, F>(f: F) -> impl Fn(&T) -> bool
@@ -13,6 +13,18 @@ pub fn not<T, F>(f: F) -> impl Fn(&T) -> bool
 pub fn confirmed_and_specific_talks(specific_talks: Vector<String>) -> impl Fn(&Talk) -> bool {
     move |talk| {
         confirmed_talks(talk)
+            || specific_talks.contains(&talk.title)
+    }
+}
+
+pub fn status(statuses: Vector<String>) -> impl Fn(&Talk) -> bool {
+    move |talk| statuses.contains(&talk.state)
+}
+
+pub fn status_and_specific_talks(statuses: Vector<String>, specific_talks: Vector<String>) -> impl Fn(&Talk) -> bool {
+    let status_filter= status(statuses);
+    move |talk| {
+        status_filter(talk)
             || specific_talks.contains(&talk.title)
     }
 }
